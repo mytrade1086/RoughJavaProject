a70f0577c037d19b49c53112c910eb9e07315645
sunshine <mytrade1086@gmail.com>
2020-02-17 17:49:07 +0530

    Recussion

---
+++ src/Testng1.java
@@ -0,0 +1,13 @@
+
+import org.testng.annotations.*;
+
+public class Testng1 {
+	
+	
+	@Test
+	public void testngONE() {
+		
+		
+	}
+
+}
--- src/factorial.java
+++ src/factorial.java
@@ -2,17 +2,25 @@
 public class factorial {
 	
 	public static void main(String[] args) {
-		
-		
 		int num=5;
 		int sum=1;
 		for(int i=num;i>0;i--) {
-			
-			
 			 sum=sum*i;
 		}
 				
 		System.out.println(sum);
 	}
+
+	public int recursion(int number) {	
+		int sum=1;
+		if (number>1) {
+			sum=sum*number;
+			number--;
+			recursion(number);
+			
+		}
+		return number;
+	}
+	
 
 }
