55d5de765d9f7d0f423e3afa6410e08f6de8b4b0
sunshine <mytrade1086@gmail.com>
2020-03-05 22:26:53 +0530

    update

--- src/swapString.java
+++ src/swapString.java
@@ -8,7 +8,22 @@ public class swapString {
 		a=a+b;	
 		b=a.replaceAll(b, "");
 		a=a.replaceFirst(b, "");
-		System.out.println("b is"+b+"   a is:"+a);
+		System.out.println("b is "+b+"   a is: "+a);
+		
+		
+		
+//		
+//		System.out.println("Below uses StringBuffer");
+//		
+//		StringBuffer sb1=new StringBuffer("sumit");
+//		StringBuffer sb2=new StringBuffer("sagar");
+//		
+//		sb1=sb1.append(sb2);
+//		
+//		System.out.println("sb1 is "+sb1);
+//		
+//		sb2=sb1.replace(sb1.indexOf("sagar"), sb1.length(), "");  // sumitsagar sagar
+		
 	}
 
 }
